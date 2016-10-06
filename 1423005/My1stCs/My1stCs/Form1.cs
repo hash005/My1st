c37614b688727298ab8643be9fe474dc387935fc
hash005 <s1423005@cce.kanagawa-it.ac.jp>
2016-10-07 06:39:36 +0900

    test

---
+++ 1423005/My1stCs/My1stCs/Form1.cs
@@ -0,0 +1,26 @@
+﻿/*
+    Form1 cs
+    初めてのC#プログラミング
+    ボタンを押すと現在時刻が表示されます
+    Created at 2016/10/4
+    Created by hibiki hashiguchi
+ */
+
+using System;
+using System.Windows.Forms;
+
+namespace My1stCs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = DateTime.Now.ToString();
+        }
+    }
+}
---
+++ 1423005/mycounter/mycounter/Form1.cs
@@ -0,0 +1,35 @@
+﻿/*
+    Form1 cs
+    C#プログラミング
+    カウンター
+    Created at 2016/10/4
+    Created by hibiki hashiguchi
+ */
+using System;
+using System.Windows.Forms;
+
+namespace mycounter
+{
+    public partial class Form1 : Form
+    {
+        // int counter;
+        // プロパティ記法
+        int Counter { get; set; }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Counter++;
+            label1.Text = Counter.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Counter = 0;
+            label1.Text = Counter.ToString();
+        }
+    }
+}
